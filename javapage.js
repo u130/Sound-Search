3583ff8db68e55d1ff626eb0ca706df21feafb65
Usman Adeel <adeel@citypolyhs.org>
2018-05-30 20:52:51 +0000

    Final Product

--- javapage.js
+++ javapage.js
@@ -1,2 +1,33 @@
 /*global $*/
-//http://api.soundcloud.com/tracks/123
+    function appendSongToBody(srcURL) {
+        $('.text-center').append('<img src=' + srcURL + '>');
+    }
+$.ajax({
+    url: "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=e8a485ebf273480961d6b26b62127701&artist=" + artistName + "&track=" + songName + "&format=json",
+    method: "GET",
+    success: function(response){
+        
+    }
+});
+
+function urlWithTerms(term) {
+    
+}
+
+function getSongNameAndArtist(name, artist) {
+    $("#bravo").click(function(){
+        var artistName = $("#trackInfo").val();
+        var songName = $("#artistInfo").val();
+    });
+   
+}
+
+    $("#bravo").click(function(songName){
+   var searchTerm = $("input").val();       
+    });
+
+    // $("button").click(function(searchTerm) {
+    //     var searchTerm = $("input").val();
+    //     callGiphyAPIWithSearchTerm(searchTerm);
+    //     $('.text-center').empty();
+    // });
